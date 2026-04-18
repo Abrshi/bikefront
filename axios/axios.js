b06856ac8c54f42bc79af1572323307fed19f18e
Abreham Yeshitla <abrhamyeshitla990@gmail.com>
2026-04-17 15:30:52 -1200

    auth middlewer

--- axios/axios.js
+++ axios/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const axiosbaseurl = axios.create({
   
- baseURL:' http://localhost:5500/api/v1',
+ baseURL:'https://bikeback.onrender.com//api/v1',
   withCredentials: true, // 
 });
 
